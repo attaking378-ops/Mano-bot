a35ea730f3e535b7f5d2366b25ac9d06384eefa1
attaking378-ops <attaking378@gmail.com>
2025-12-25 16:31:59 +0500

    Update API keys in 0_RDX_GOIBOT.js

--- rdx/commands/0_RDX_GOIBOT.js
+++ rdx/commands/0_RDX_GOIBOT.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const CEREBRAS_API_URL = 'https://api.cerebras.ai/v1/chat/completions';
 
-const API_KEYS = ['ADD YOUR API KEYS HERE - GET FROM https://cerebras.ai./'];
+const API_KEYS = ['apim_5UVmpLdCVx5rcMJsVrjHG_RUiyTqk8CL5UASf2Hm4oc'];
 
 const OWNER_UID = '100002392368552';
 const OWNER_NAME = 'Attaullah';
