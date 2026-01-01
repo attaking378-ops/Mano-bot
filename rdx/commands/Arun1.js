1ddc369cc180246fc6ea70e64568f9e9376a67df
attaking378-ops <attaking378@gmail.com>
2026-01-01 15:03:59 +0500

    Update Arun1.js

--- rdx/commands/Arun1.js
+++ rdx/commands/Arun1.js
@@ -1,6 +1,6 @@
  const fs = require("fs");
 module.exports.config = {
-	name: "Arun",
+	name: "Atta",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "Arun", 
@@ -14,10 +14,10 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("owner") ||
-     react.includes("Arun") || 
-react.includes("arun")) {
+     react.includes("atta") || 
+react.includes("Attaullah")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐫𝐮𝐧 𝐊𝐮𝐦𝐚𝐫✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐭𝐭𝐚𝐮𝐥𝐥𝐚𝐡 𝐊𝐢𝐧𝐠✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/attaullah ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 \n 𝐒𝐢𝐧𝐝𝐡𝐢 😋https://www.facebook.com/khuharo`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
